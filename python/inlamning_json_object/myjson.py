f62d64786ba534173f9e4b51697439233bb56177
Kamjar Gerami <kami.gerami@gmail.com>
2017-10-22 12:16:17 +0200

    python: unittest passes

--- python/inlamning_json_object/myjson.py
+++ python/inlamning_json_object/myjson.py
@@ -1,10 +1,25 @@
 import sys
+import re
 
 if sys.version_info[0] < 3:
         raise Exception("Python 3.6 or a more recent version is required for all tests to pass.")
 
 
+def multiple_replace(myregex, data):
+	# Create a regular expression from the dictionary keys
+	regex = re.compile("(%s)" % "|".join(map(re.escape, myregex.keys())))
+	# For each match, look-up corresponding value in dictionary
+	return regex.sub(lambda mo: myregex[mo.string[mo.start():mo.end()]], str(data))
+
 def json_encode( data ):
+    myregex = {
+        '\''   : '"',
+        ', '   : ',',
+        'False': 'false',
+        'True' : 'true',
+        ': ' : ':',
+	}
+
     if isinstance( data, bool ):
         if data:
             return "true"
@@ -15,17 +30,15 @@ def json_encode( data ):
             # it passes however in python 3.5+
             return str(data)
     elif isinstance( data, str ):
-            return '"{}"'.format(data.replace('\n', '\\n'))
+            return '"{}"'.format(str(data).replace('\n', '\\n'))
     elif isinstance( data, list ):
-              return str(data).replace(' ', '').replace("'", '"').replace('False', 'false').replace('True', 'true')
+              return multiple_replace(myregex, data)
     elif isinstance( data, dict ):
             # this test fails in python 2.7 && 3.5 however passes in 3.6+
             # does 3.6 include some automatic sorting of dicts maybe?
-            result_dict = {}
-            for key, value in data.items():
-                  result_dict[key] = value
-            return str(result_dict).replace(': ', ':').replace(', ', ',').replace("'", '"').replace('False', 'false').replace('True', 'true')
-
+            # regex fixes the "October 13, 2012" comma whitespace removal from myregex dict.
+            return re.sub('(.*\".\"\w.+[0-9]{1,4},)([0-9]{1,4}\")',
+                          '\\1 \\2', multiple_replace(myregex, data))
     else:
         # All other types do not  need to be implemented - it is OK that they raise an error
         raise TypeError( "%s is not JSON serializable" % repr( data ) )
